f652aca50cb9b8a51dc9be9386b6d10f5db2982b
viveknathani <viveknathani2402@gmail.com>
2024-02-11 04:42:38 +0530

    feat: serve static

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
-console.log(process.env.PORT);
+import express from 'express';
+import path from 'path';
+
+type ExpressFunction = (
+  req: express.Request,
+  res: express.Response,
+) => Promise<void>;
+
+function serveFromPath(webPagePath: string): ExpressFunction {
+  return async (req: express.Request, res: express.Response) => {
+    try {
+      res.sendFile(path.resolve(__dirname, webPagePath));
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ message: '' });
+    }
+  };
+}
+
+async function main() {
+  const app = express();
+  const port = process.env.PORT || 3000;
+  app.use('/static', express.static(path.join(__dirname, './static')));
+  app.use('/', serveFromPath('./static/index.html'));
+  app.listen(port, () => {
+    console.log('⚡️ server is up and running!');
+  });
+}
+
+main();
