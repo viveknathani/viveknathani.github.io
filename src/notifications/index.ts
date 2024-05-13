c701d3c420ec7439f2f905aaf4f0084ccd525c4b
viveknathani <viveknathani2402@gmail.com>
2024-05-13 16:06:06 +0530

    chore: onboarding second user :p

--- src/notifications/index.ts
+++ src/notifications/index.ts
@@ -5,18 +5,26 @@ import { NOTIFICATION_CHANNEL, NotificationRequest } from '../types';
 async function sendNotification(request: NotificationRequest) {
   switch (request.channel) {
     case NOTIFICATION_CHANNEL.TELEGRAM: {
-      await axios.post(
-        `https://api.telegram.org/bot${config.TELEGRAM_API_KEY}/sendMessage`,
-        {
-          chat_id: 5501101308,
-          text: request.data.text,
-          disable_web_page_preview: true,
-        },
-        {
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        },
+      const chatIds = [
+        5501101308, // vivek
+        1204630303, // advait
+      ];
+      await Promise.all(
+        chatIds.map(async (chatId) => {
+          await axios.post(
+            `https://api.telegram.org/bot${config.TELEGRAM_API_KEY}/sendMessage`,
+            {
+              chat_id: chatId,
+              text: request.data.text,
+              disable_web_page_preview: true,
+            },
+            {
+              headers: {
+                'Content-Type': 'application/json',
+              },
+            },
+          );
+        }),
       );
       break;
     }
